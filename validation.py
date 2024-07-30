aec99e0b3a9b2043f9d2f5d9114e4a12ddf271a5
Diego Trujillo <42224996+DiegoTrux@users.noreply.github.com>
2024-07-29 22:22:32 -0500

    Add files via upload

---
+++ validation.py
@@ -0,0 +1,9 @@
+import os
+from typing import Any, Text, Dict, List
+import pandas as pd
+import requests
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+print("Todas las librerías están instaladas correctamente.")
